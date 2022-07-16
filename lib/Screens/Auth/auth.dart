e09b60cc679994183dda63333df50457ee7c667f
Darksister <fatema@ursoft.com>
2022-07-16 10:51:05 +0300

    New SCREENS

---
+++ lib/Screens/Auth/auth.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  Authenticate({Key? key}) : super(key: key);
+
+  @override
+  State<Authenticate> createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('login');
+  }
+}
---
+++ lib/Screens/home/home.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Home');
+  }
+}
